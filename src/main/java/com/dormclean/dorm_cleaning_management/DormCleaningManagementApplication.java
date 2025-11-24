1ee782ddcdb0d4d1099780af56517dd9ef4ed5e4
Glory0206 <dmb07223@gmail.com>
2025-11-24 23:07:26 +0900

    chore: 초기 세팅

---
+++ src/main/java/com/dormclean/dorm_cleaning_management/DormCleaningManagementApplication.java
@@ -0,0 +1,13 @@
+package com.dormclean.dorm_cleaning_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormCleaningManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormCleaningManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dormclean/dorm_cleaning_management/DormCleaningManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dormclean.dorm_cleaning_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormCleaningManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
