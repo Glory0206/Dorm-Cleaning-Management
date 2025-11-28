509cb0eb93f6ed2e6ca14a89858be4da74b34342
Glory0206 <dmb07223@gmail.com>
2025-11-28 11:31:29 +0900

    feat: 이미 코드가 존재한다면 덮어쓰기

--- src/main/java/com/dormclean/dorm_cleaning_management/entity/CleaningCode.java
+++ src/main/java/com/dormclean/dorm_cleaning_management/entity/CleaningCode.java
@@ -27,4 +27,8 @@ public class CleaningCode {
         this.dorm = dorm;
         this.code = code;
     }
+
+    public void updateCode(String code){
+        this.code = code;
+    }
 }
--- src/main/java/com/dormclean/dorm_cleaning_management/service/CleaningCodeServiceImpl.java
+++ src/main/java/com/dormclean/dorm_cleaning_management/service/CleaningCodeServiceImpl.java
@@ -6,6 +6,9 @@ import com.dormclean.dorm_cleaning_management.repository.CleaningCodeRepository;
 import com.dormclean.dorm_cleaning_management.repository.DormRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,13 +17,22 @@ public class CleaningCodeServiceImpl implements  CleaningCodeService {
     private final DormRepository dormRepository;
 
     @Override
+    @Transactional
     public void registration(String dormCode, String cleaningCode) {
         Dorm dorm = dormRepository.findByDormCode(dormCode).orElseThrow(() -> new IllegalArgumentException("해당 기숙사가 존재하지 않습니다."));
-        CleaningCode cleaningcode = CleaningCode.builder()
-                .dorm(dorm)
-                .code(cleaningCode)
-                .build();
 
-        cleaningCodeRepository.save(cleaningcode);
+        Optional<CleaningCode> existingCode = cleaningCodeRepository.findByDorm(dorm);
+
+        if (existingCode.isPresent()) {
+            // 이미 있다면 덮어쓰기
+            existingCode.get().updateCode(cleaningCode);
+        } else {
+            // 없다면 새로 저장
+            CleaningCode newCleaningCode = CleaningCode.builder()
+                    .dorm(dorm)
+                    .code(cleaningCode)
+                    .build();
+            cleaningCodeRepository.save(newCleaningCode);
+        }
     }
 }
