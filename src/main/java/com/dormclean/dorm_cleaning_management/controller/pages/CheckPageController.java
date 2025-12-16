31a0d97deaaa9a6ffdcd344f914199c59823c93c
Seokyun <jangsy0623@naver.com>
2025-12-16 09:49:17 +0900

    refactor : check페이지 이름 복구

--- src/main/java/com/dormclean/dorm_cleaning_management/controller/pages/CheckPageController.java
+++ src/main/java/com/dormclean/dorm_cleaning_management/controller/pages/CheckPageController.java
@@ -14,13 +14,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
-public class CheckAuthPageController {
+public class CheckPageController {
 
     private final QrCodeService qrCodeService;
     private final DormRepository dormRepository;
     private final RoomRepository roomRepository;
 
-    @GetMapping("/check-auth")
+    @GetMapping("/check")
     public String check(@RequestParam("token") String token, Model model) {
         // 서비스에서 토큰으로 기숙사/호실 정보 가져오기
         QrResponseDto data = qrCodeService.getQrData(token);
@@ -36,6 +36,6 @@ public class CheckAuthPageController {
         model.addAttribute("status", room.getRoomStatus().name());
 
         // check.html 파일을 보여줌
-        return "check-auth";
+        return "check";
     }
 }
