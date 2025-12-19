7de8d2d560c18fa44625bcebf42295c6f14488f8
Seokyun <jangsy0623@naver.com>
2025-12-19 09:17:10 +0900

    feat : 계정 등록 기능 구현

--- src/main/java/com/dormclean/dorm_cleaning_management/controller/AdminController.java
+++ src/main/java/com/dormclean/dorm_cleaning_management/controller/AdminController.java
@@ -4,23 +4,37 @@ import lombok.RequiredArgsConstructor;
 
 import jakarta.validation.Valid;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dormclean.dorm_cleaning_management.dto.admin.AccountListResponseDto;
 import com.dormclean.dorm_cleaning_management.dto.admin.AdminLoginRequestDto;
+import com.dormclean.dorm_cleaning_management.entity.enums.UserRole;
 import com.dormclean.dorm_cleaning_management.service.admin.UserService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/admin/api")
 @RequiredArgsConstructor
 @Tag(name = "유저 관리 API", description = "유저 생성 및 삭제 처리 API")
 public class AdminController {
     private final UserService userService;
 
+    // ADMIN 계정 조회
+    @GetMapping("/accounts")
+    public ResponseEntity<List<AccountListResponseDto>> getAdminAccounts() {
+        List<AccountListResponseDto> dtoList = userService.AllAdminAccounts(UserRole.ADMIN);
+        return ResponseEntity.ok(dtoList);
+    }
+
+    // 계정 등록
     @PostMapping("/signup")
     public String signup(@Valid @RequestBody AdminLoginRequestDto dto) {
         userService.create(dto.username(), dto.password());
