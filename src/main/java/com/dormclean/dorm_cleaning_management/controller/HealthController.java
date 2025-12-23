f58e6e4d83fe1fb1881d5783e269c68fdc4af61e
Glory0206 <dmb07223@gmail.com>
2025-12-23 16:48:50 +0900

    fix: 잘못된 어노테이션 수정

--- src/main/java/com/dormclean/dorm_cleaning_management/controller/HealthController.java
+++ src/main/java/com/dormclean/dorm_cleaning_management/controller/HealthController.java
@@ -1,11 +1,11 @@
 package com.dormclean.dorm_cleaning_management.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @CrossOrigin("*")
 public class HealthController {
     @GetMapping("health")
